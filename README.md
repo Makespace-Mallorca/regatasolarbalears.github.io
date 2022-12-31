8c669e6bfd40a2049e5f62aba5d7228ad626f641
Carlos Orts <mcorts69@gmail.com>
2022-12-31 13:27:20 +0100

    Initial

---
+++ README.md
@@ -0,0 +1,19 @@
+# [IoT Foundations website](http://iot-foundations.github.io/)
+
+[![License](http://img.shields.io/:license-gpl-blue.svg)](http://opensource.org/licenses/GPL-2.0)
+
+Based on [Start Bootstrap Agency Jekyll theme](https://github.com/y7kim/agency-jekyll-theme).
+
+### Development
+
+Execute `jekyll serve` to run the website locally.
+
+#### Structure
+
+* `_content`: contains the keys and links of the Projects-Tutorials-Workshops-Talks.
+* `_data\strings.yml`: contains all the strings translated (en, es).
+* `img`: contains all the images. It is recommended to use the size 600x510px:
+
+Projects & Tutorials: `convert input.png -resize 600 -background "rgba(0,0,0,0)" -gravity center -extent 600x510 output.png`
+
+Workshops & Talks: `convert input.png -resize 550 -background "rgba(0,0,0,0)" -gravity center -extent 600x510 output.png`
---
+++ js/agency.js
@@ -0,0 +1,76 @@
+/*
+ * IoT Foundations (c) 2016-2022
+ * License: GPL-2.0
+ * Credits: Start Bootstrap - Agnecy Bootstrap Theme (http://startbootstrap.com)
+ */
+
+'use strict';
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1000, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+// Highlight the top nav as scrolling occurs
+$('body').scrollspy({
+    target: '.navbar-fixed-top'
+});
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
+
+// Language management
+
+var lang = '';
+var languages = ['en', 'es'];
+var preferredLanguages = navigator.languages || [navigator.language];
+var detectedLang = 'es';// Default if none of the user’s languages is available.
+preferredLanguages.some(function (lang) {
+    lang = lang.split('-')[0];
+    if (languages.indexOf(lang) !== -1) {
+        detectedLang = lang;
+        return true;
+    } else {
+        return false;
+    }
+});
+
+// Set detected language
+loadLanguage(detectedLang);
+
+$('#lang-en').click(function(/*event*/) {
+  loadLanguage('en', true);
+});
+
+$('#lang-es').click(function(/*event*/) {
+  loadLanguage('es', true);
+});
+
+function loadLanguage(newLang, moveTop) {
+  if (lang !== newLang) {
+    // Language changed
+    for (var i in languages) {
+      if (languages[i] === newLang) {
+        $('.' + languages[i]).removeClass('hidden');
+      }
+      else {
+        $('.' + languages[i]).addClass('hidden');
+      }
+      lang = newLang;
+    }
+    if (moveTop) {
+      // Move to top
+      $('html, body').stop().animate({
+        scrollTop: 0
+      }, 1000, 'easeInOutExpo');
+    }
+  }
+}
